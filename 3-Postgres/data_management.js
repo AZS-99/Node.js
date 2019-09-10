97bdac845a4effd5468a03588da7ed81d8ddf93b
AZS-99 <sadam4@myseneca.ca>
2019-09-10 17:09:16 -0400

    Comment to illustrate how to use Sequelise

--- 3-Postgres/data_management.js
+++ 3-Postgres/data_management.js
@@ -1,4 +1,10 @@
 const Sequelise = require('sequelize')
+/*
+const database = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect:  one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' 
+  });
+*/
 const database = new Sequelise('d7cgiig48727gm', 'jytkynsmmdequz', '7555b9f64c706ca65c08646f37ca50b1e8234f3df1d6003548db4f28256184b5', {
     host: 'ec2-50-19-114-27.compute-1.amazonaws.com',
     port: '5432',
