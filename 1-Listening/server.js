d4ba7dc73efb13b90175fb88bd86b2927101093c
Adam Saher <sadam4@myseneca.ca>
2020-07-04 20:34:21 -0400

    delete env from http_port

--- 1-Listening/server.js
+++ 1-Listening/server.js
@@ -1,6 +1,6 @@
 const express = require('express')           //require is the equivalent of 'include' in C++
 
-const http_port = process.env.PORT || 8080
+const http_port = 8080
 const app = express();                       //app is an instance of class express
 
 app.get('/', (req, res) => {
