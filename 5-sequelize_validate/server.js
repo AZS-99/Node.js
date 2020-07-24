0ac378c981f78be9e457a65234a6c107cfa6c362
Adam <adam@Adams-MacBook-Pro.local>
2020-07-23 23:41:33 -0400

    Sequelize-validate

---
+++ 5-sequelize_validate/server.js
@@ -0,0 +1,51 @@
+const body_parser = require('body-parser')
+const database = require('./database')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+
+const app = express() 
+
+//Define express-handlebars to express
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+//In order to avoid specifying the extension '.hbs' every single time
+app.set('view engine', 'hbs')
+app.use(body_parser.urlencoded({extended: true}))
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home'
+    })
+})
+
+
+app.get('/sign_up', (req, res) => {
+    res.render('sign_up')
+})
+
+
+app.post('/sign_up', async (req, res) => {
+    try {
+        await database.add_user(req.body)
+        res.send("User added successful")
+    } catch (error) {
+        throw(error)
+    }
+})
+
+
+
+
+;(async () => {
+    try {
+        await database.initalise()
+        app.listen(8080)
+    } catch(error) {
+        console.log(error)
+    }
+}) ()
