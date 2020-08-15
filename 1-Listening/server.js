e846bbf74e3ece4358d7b8ada265f8e2726c3d8f
Adam <adam@Adams-MacBook-Pro.local>
2020-08-15 10:44:17 -0400

    .env

--- 1-Listening/server.js
+++ 1-Listening/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')           //require is the equivalent of 'include' in C++
 
-const http_port = 8080
+const http_port = process.env.HTTP_PORT
 const app = express();                       //app is an instance of class express
 
 app.get('/', (req, res) => {
