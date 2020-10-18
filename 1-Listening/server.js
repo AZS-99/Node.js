96591a3c1c1d09969739e72711ca008562bbc6eb
Adam <adam@Adams-MacBook-Pro.local>
2020-10-18 16:21:51 -0400

    Remove the redundant definition of HTTP_PORT as it is in the .env anyway

--- 1-Listening/server.js
+++ 1-Listening/server.js
@@ -1,13 +1,10 @@
 require('dotenv').config()              //require is the equivalent of #include in C++
 const express = require('express')
 
-const HTTP_PORT = process.env.HTTP_PORT
-
 const app = express()                   //app is an instance of class 'express'
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
-app.listen(HTTP_PORT)
+app.listen(process.env.HTTP_PORT)
