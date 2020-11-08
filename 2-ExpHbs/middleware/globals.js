debda1172fd16ecf35628ff0c774a8bed0cdb7cf
Adam <adam@Adams-MacBook-Pro.local>
2020-11-07 22:15:32 -0500

    removing old exphbs

--- 2-ExpHbs/middleware/globals.js
+++
@@ -1,8 +0,0 @@
-const globals = (req, res, next) => {
-    res.locals.nav_left = {"Home": "/", "About": "/about"}
-    res.locals.nav_right = {"Log in": "log_in", "Sign up": "sign_up"}
-    next()
-}
-
-
-module.exports = globals
