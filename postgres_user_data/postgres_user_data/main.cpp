36a2ed07aa71aafadeeb586ea86d335e0927b14e
Adam <adam@Adams-MacBook-Pro.local>
2020-12-20 17:14:14 -0500

    prune

---
+++ postgres_user_data/postgres_user_data/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  postgres_user_data
+//
+//  Created by Adam Saher on 2020-11-29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
