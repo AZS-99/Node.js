01a29769b49f750cd660e798cb9d93cbd1c74a07
Adam <adam@Adams-MacBook-Pro.local>
2020-12-20 17:15:54 -0500

    remove ds_store

--- postgres_user_data/postgres_user_data/main.cpp
+++ postgres_user_data/postgres_user_data/main.cpp
@@ -7,8 +7,18 @@
 
 #include <iostream>
 
+void create() {
+    std::cout << "INSERT INTO \"USERs\" (first_name, surname, email, password, \"createdAt\", \"updatedAt\")" << std::endl;
+    
+    std::cout << "VALUES ";
+    for (unsigned i = 0; i < 100; ++i) {
+        std::cout << "('first_name" << i << "', 'surname" << i << "', 'email_" << i <<
+        "@gmail.com'" << ", 'Password" << i << "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)," << std::endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    create();
     return 0;
 }
